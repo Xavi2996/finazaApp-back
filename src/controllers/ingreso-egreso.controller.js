b90dc06975d0283ab54cf6d18e4801ce6425c280
xavier <xavier.cobos.29@hotmail.com>
2024-01-11 16:40:24 -0500

    servicio de ingresos y egresos para filtar por year y month

--- src/controllers/ingreso-egreso.controller.js
+++ src/controllers/ingreso-egreso.controller.js
@@ -7,6 +7,8 @@ const getTotal = async (req, res) => {
     try {
         const [ingresos] = await ingresoModel.selectAllIngresos();
         const [egresos] = await egresoModel.selectAllEgresos();
+        console.log(ingresos);
+        
         ingresos.forEach(element => {
             ingresosTotal = ingresosTotal + Number(element.cantidad);
         });
@@ -14,16 +16,7 @@ const getTotal = async (req, res) => {
         egresos.forEach(element => {
             egresosTotal = egresosTotal + Number(element.cantidad);
         });
-        res.json({
-            respuesta: true,
-            mensaje: 'respuesta exitosa',
-            resultado: {
-                ingresos,
-                egresos,
-                ingresosTotal,
-                egresosTotal
-            } 
-        });   
+
     } catch (error) {
         res.json({
             respuesta: false,
@@ -34,15 +27,19 @@ const getTotal = async (req, res) => {
 }
 
 const getTotalDate = async (req, res) => {
-    console.log(req.body);
     const {year, id, month} = req.body
     let ingresosTotal = 0;
     let egresosTotal = 0;
+    let ingresosTotalYear = 0;
+    let egresosTotalYear = 0;
     console.log(req.body);
     
     try {
         const [ingresos] = await ingresoModel.selectIngresosMonth(year, id, month);
         const [egresos] = await egresoModel.selectEgresosMonth(year, id, month);
+        const [ingresosYear] = await ingresoModel.selectIngresosYear(year, id);
+        const [egresosYear] = await egresoModel.selectEgresosYear(year, id);
+
         ingresos.forEach(element => {
             ingresosTotal = ingresosTotal + Number(element.cantidad);
         });
@@ -50,16 +47,39 @@ const getTotalDate = async (req, res) => {
         egresos.forEach(element => {
             egresosTotal = egresosTotal + Number(element.cantidad);
         });
-        res.json({
-            respuesta: true,
-            mensaje: 'respuesta exitosa',
-            resultado: {
-                ingresos,
-                egresos,
-                ingresosTotal,
-                egresosTotal
-            } 
-        });   
+    
+        ingresosYear.forEach(element => {
+            ingresosTotalYear = ingresosTotalYear + Number(element.cantidad);
+        });
+
+        egresosYear.forEach(element => {
+            egresosTotalYear = egresosTotalYear + Number(element.cantidad);
+        });
+
+        if (month == 0) {
+            res.json({
+                respuesta: true,
+                mensaje: 'respuesta exitosa',
+                resultado: {
+                    ingresos: ingresosYear,
+                    egresos: egresosYear,
+                    ingresosTotal : ingresosTotalYear,
+                    egresosTotal : egresosTotalYear
+                }
+            });  
+        } else {
+            res.json({
+                respuesta: true,
+                mensaje: 'respuesta exitosa',
+                resultado: {
+                    ingresos,
+                    egresos,
+                    ingresosTotal,
+                    egresosTotal
+                } 
+            });       
+        }
+
     } catch (error) {
         res.json({
             respuesta: false,

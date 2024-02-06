0b957141db0f7f84edff22ee2e6613770f0d02ca
xavier <xavier.cobos.29@hotmail.com>
2024-02-06 18:08:50 -0500

    Servicio de delete categoría

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -3,7 +3,6 @@ const usersModel = require('../models/users.model')
 const getSpecificUser = async(req, res) => {
     try {
         const [result] = await usersModel.selectSpecificUser();
-        console.log(result);
         res.json({
             respuesta: true,
             mensaje: 'Usuario recuperado con éxito',
@@ -17,4 +16,125 @@ const getSpecificUser = async(req, res) => {
     }
 }
 
-module.exports = {getSpecificUser}
+const getCatIngreso = async(req, res) => {
+    try {
+        const [result] = await usersModel.selectCatIngreso();
+        res.json({
+            respuesta: true,
+            mensaje: 'Categorías de ingreso obtenidas con éxito',
+            resultado: result
+        });   
+    } catch (error) {
+        res.json({
+            respuesta: false,
+            mensaje: 'No se encontraro categorías de ingreso',
+        })
+    }
+}
+
+const getCatEgreso = async(req, res) => {
+    try {
+        const [result] = await usersModel.selectCatEgreso();
+        console.log(result);
+        res.json({
+            respuesta: true,
+            mensaje: 'Categorías de egreso obtenidas con éxito',
+            resultado: result
+        });   
+    } catch (error) {
+        res.json({
+            respuesta: false,
+            mensaje: 'No se encontraro categorías de egreso',
+        })
+    }
+}
+
+const postCatIngreso = async(req, res) => {
+    try {
+        const [result] = await usersModel.insertCatIngresos(req.body.categoria);
+        const [respuesta] = await usersModel.selectCatIngreso();
+        res.json({
+            respuesta: true,
+            mensaje: 'Categorías de ingreso obtenidas con éxito',
+            resultado: respuesta
+        });
+    } catch (error) {
+        console.log(error.code);
+        if (error.code == 'ER_DUP_ENTRY') {
+            res.json({
+                respuesta: false,
+                mensaje: 'La categoría ya existe',
+                resultado: null
+            });   
+        }
+    }
+}
+
+const postCatEgreso = async(req, res) => {
+    try {
+        const [result] = await usersModel.insertCatEgresos(req.body.categoria);
+        const [respuesta] = await usersModel.selectCatEgreso();
+        res.json({
+            respuesta: true,
+            mensaje: 'Categorías de egreso obtenidas con éxito',
+            resultado: respuesta
+        });
+    } catch (error) {
+        console.log(error.code);
+        if (error.code == 'ER_DUP_ENTRY') {
+            res.json({
+                respuesta: false,
+                mensaje: 'La categoría ya existe',
+                resultado: null
+            });   
+        }
+    }
+}
+
+const deleteCatIngreso = async (req, res) => {
+    console.log(req.params);
+    try {
+        const [result] = await usersModel.deleteCatIngresos(req.params.id);
+        const [respuesta] = await usersModel.selectCatIngreso();
+        
+        res.json({
+            respuesta: true,
+            mensaje: 'Categoría eliminada con éxito',
+            resultado: respuesta
+        });
+    } catch (error) {
+        console.log(error.code);
+        if (error.code == 'ER_DUP_ENTRY') {
+            res.json({
+                respuesta: false,
+                mensaje: 'La categoría ya existe',
+                resultado: null
+            });   
+        }
+    }
+}
+
+const deleteCatEgreso = async (req, res) => {
+    console.log(req.params);
+    try {
+        const [result] = await usersModel.deleteCatEgresos(req.params.id);
+        const [respuesta] = await usersModel.selectCatEgreso();
+        
+        res.json({
+            respuesta: true,
+            mensaje: 'Categoría eliminada con éxito',
+            resultado: respuesta
+        });
+    } catch (error) {
+        console.log(error.code);
+        if (error.code == 'ER_DUP_ENTRY') {
+            res.json({
+                respuesta: false,
+                mensaje: 'La categoría ya existe',
+                resultado: null
+            });   
+        }
+    }
+}
+
+module.exports = {getSpecificUser, getCatIngreso, getCatEgreso,postCatIngreso, postCatEgreso,deleteCatIngreso,deleteCatEgreso}
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -3,6 +3,12 @@ const router = require('express').Router();
 const usersController = require('../../controllers/users.controller');
 
 router.get('/', usersController.getSpecificUser);
+router.get('/categoria-ingreso', usersController.getCatIngreso);
+router.get('/categoria-egreso', usersController.getCatEgreso);
+router.post('/insertCatIngreso', usersController.postCatIngreso);
+router.post('/insertCatEgreso', usersController.postCatEgreso);
+router.get('/deleteCatIngreso/:id', usersController.deleteCatIngreso);
+router.get('/deleteCatEgreso/:id', usersController.deleteCatEgreso);
 
 
 module.exports = router;
